dc2d31a3fc2b3b6c605c201f43b12d3cd36363b3
rugk <rugk@posteo.de>
2018-11-19 13:11:56 +0100

    Adjust lodash paths

--- tests/helper/FakeStorage.js
+++ tests/helper/FakeStorage.js
@@ -1,5 +1,5 @@
-import isPlainObject from "../lodash/isPlainObject.js";
-import isString from "../lodash/isString.js";
+import isPlainObject from "../../../lodash/isPlainObject.js";
+import isString from "../../../lodash/isString.js";
 
 // NOTE: When using, remember this uses "this". So better .bind(thisObject) here.
 
