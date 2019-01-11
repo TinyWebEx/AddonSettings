5e43e528c5ea35e145b61d743c2f86341686c44d
rugk <rugk@posteo.de>
2019-01-11 10:22:55 +0100

    Add unit tests for default settings

--- tests/run.js
+++ tests/run.js
@@ -2,6 +2,7 @@ import "https://unpkg.com/mocha@5.2.0/mocha.js"; /* globals mocha */
 
 /* tests */
 import "./addonSettings.test.js";
+import "./dataTest/defaultSettings.test.js";
 
 mocha.checkLeaks();
 mocha.run();
